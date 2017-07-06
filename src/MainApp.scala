7367acc2665bccf3f1c7ed04c82e78844a26a9f0
o m <sandrm@ukr.net>
2017-07-06 14:01:19 +0300

    Merge branch 'features/Test17'
    fixed
    # Conflicts:
    #	src/MainApp.scala
    #	src/StartApp.scala

--- src/MainApp.scala
+++ src/MainApp.scala
@@ -1,3 +1,5 @@
+import simple.SimpleClass
+
 /**
   * Created by sandr on 28.05.2017.
   */
@@ -13,5 +15,30 @@ object MainApp {
     //Done only local commit  to /Test17
 
     //merged it seems locally
+
+ //   val simpleClass = {
+//      new SimpleClass(addCalc(5, 7))
+//    }
+
+
+    var constructorsExample = new ConstructorsExample("This is parameter to instance.")
+    constructorsExample.insatnceMethod1()
+
+    ConstructorsExample("use simple constructor")
+
+    ConstructorsExample("Paramert 1", 123456)
+
+    ConstructorsExample("Paramert 1").st
   }
+
+
+  def print(line:String): Unit ={
+    System.out.println(line)
+  }
+
+  def addCalc(x:Int, y:Int): Int = {
+    x + y
+  }
+
+  //This is branch Test17_patch
 }
